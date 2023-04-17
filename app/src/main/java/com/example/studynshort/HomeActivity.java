2178ab6feb7844463f26643becb326cffd93f7ea
LENOVO <2020056774@dkasc.ac.in>
2023-04-17 12:21:06 +0530

    Update

--- app/src/main/java/com/example/studynshort/HomeActivity.java
+++ app/src/main/java/com/example/studynshort/HomeActivity.java
@@ -10,7 +10,6 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
 
-    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
