c25ec0f024df3599041fe7b2e94258d4782a972d
LENOVO <2020056774@dkasc.ac.in>
2023-04-17 12:25:08 +0530

    Update

--- app/src/main/java/com/example/studynshort/HomeActivity.java
+++ app/src/main/java/com/example/studynshort/HomeActivity.java
@@ -15,11 +15,6 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
-            Window window = this.getWindow();
-            window.setStatusBarColor(this.getResources().getColor(R.color.top));
-        }
-
 
 
 
